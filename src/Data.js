a9ce7194ef8a103ce050c07119af7e7a074b735e
costareebsbwa@gmail.com <costareebsbwa@gmail.com>
2023-02-17 09:25:25 +0200

    god bless

--- src/Data.js
+++ src/Data.js
@@ -1,86 +1,99 @@
-let dataItems=[
-    {
-      id: "jfhgbvnscs",
-      name: "Jeleuri Croco",
-      price: 45,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-1.jpeg",
-    },
-    {
-      id: "ioytrhndcv",
-      name: "Brățară de Crocodil",
-      price: 100,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-2.jpeg",
-    },
-    {
-      id: "wuefbncxbsn",
-      name: "Croco de ciocolată",
-      price: 25,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-3.jpeg",
-    },
-    {
-      id: "bfwrbfrkefberu",
-      name: "Croco Plantă",
-      price: 300,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-4.jpeg",
-    },
-    {
-      id: "thiecbawdjksadjk",
-      name: "Schelete Croco Verde",
-      price: 25,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-5.jpeg",
-    },
-    {
-      /* proprietate: valoare,*/ id: "iuertrywebncdjksadjk",
-      name: "Schelete Croco Roz",
-      price: 200,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-6.jpeg",
-    },
-    {
-      id: "thierytbvcbvzdhadjk",
-      name: "Gummy Croco",
-      price: 450,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-7.jpeg",
-    },
-    {
-      id: "trfoiwfcnbcawdjksadjk",
-      name: "Biscuiți Croco",
-      price: 45,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-8.jpeg",
-    },
-    {
-      id: "cbvxbcvsceldk",
-      name: "Croco Plușul",
-      price: 85,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-9.jpeg",
-    },
-    {
-      id: "oiopijmjkhuihb",
-      name: "Croco scărpinători",
-      price: 120,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-10.jpeg",
-    },
-    {
-      id: "oiopijewyiohbjhib",
-      name: "Croco Căciulă",
-      price: 35,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-11.jpeg",
-    },
-    {
-      id: "rtytytuyuytyytbvncv",
-      name: "Croco Tricotatul",
-      price: 350,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-12.jpg",
-    },
-]
+/*
+Acesta este uhn JSON. Dar la ce ne ajuta un JSON?
+
+Formatul JSON este similar din punct 
+de vedere sintactic cu codul utilizat 
+la crearea obiectelor. 
+Din acest motiv, un program JavaScript 
+poate converti cu ușurință datele JSON
+în obiecte JavaScript, acestea fiind toate la un loc,
+pentru o modificare a datelor mult mai usoara, 
+atunci cand este necesar.
+*/
+
+let dataItems = [
+  {
+    id: "jfhgbvnscs",
+    name: "Jeleuri Croco",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpeg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Brățară de Crocodil",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpeg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Croco de ciocolată",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpeg",
+  },
+  {
+    id: "bfwrbfrkefberu",
+    name: "Croco Plantă",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpeg",
+  },
+  {
+    id: "thiecbawdjksadjk",
+    name: "Schelete Croco Verde",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-5.jpeg",
+  },
+  {
+    /* proprietate: valoare,*/ id: "iuertrywebncdjksadjk",
+    name: "Schelete Croco Roz",
+    price: 200,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-6.jpeg",
+  },
+  {
+    id: "thierytbvcbvzdhadjk",
+    name: "Gummy Croco",
+    price: 450,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-7.jpeg",
+  },
+  {
+    id: "trfoiwfcnbcawdjksadjk",
+    name: "Biscuiți Croco",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-8.jpeg",
+  },
+  {
+    id: "cbvxbcvsceldk",
+    name: "Croco Plușul",
+    price: 85,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-9.jpeg",
+  },
+  {
+    id: "oiopijmjkhuihb",
+    name: "Croco scărpinători",
+    price: 120,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-10.jpeg",
+  },
+  {
+    id: "oiopijewyiohbjhib",
+    name: "Croco Căciulă",
+    price: 35,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-11.jpeg",
+  },
+  {
+    id: "rtytytuyuytyytbvncv",
+    name: "Croco Tricotatul",
+    price: 350,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-12.jpg",
+  },
+];
--- src/cart.js
+++ src/cart.js
@@ -1,22 +1,49 @@
+/* Am incercat sa selectam elementele pe care dorim sa le si modificam, 
+sa le efectuam anumite transformari,
+in functie de aciunile utilizatorului, respectiv:
+- totalul -> in functie de ce produse a selectat, pretul lor si cantitatea comandata
+- produsele afisate -> trebuie afisate produsele care se afla in interiorul bag ului
+*/
+
 let label = document.getElementById("label");
 let shopping_cart = document.getElementById("shopping-cart");
 
+//Localstorage
+/*
+Ce mama mea e asta?
+In localstorage datele sunt stocate si salvate în sesiunile browserului.
+LocalStorage este similar cu sessionStorage, cu excepția faptului că,
+în timp ce datele localStorage nu au timp de expirare,  datele din
+sessionStorage sunt sterse odata ce page session ul e finalizat, 
+atunci cand pagina este inchisa
+*/
+
+//parse - pentru a parcurge JSON ul cu date, utilizam functia parse a JSON-urilor
+// || operatorul de sau; in cazul in care nu exista obiecte in json,
+//acesta e initializat ca un array gol
+
 let bag = JSON.parse(localStorage.getItem("data")) || [];
 
+//in metoda calculate() se afiseaza cate produse sunt in total in cos, cantitatea totala
+// fiind afisata la iconita de sus
 const calculate = () => {
   let carticon = document.getElementById("cartAmount");
   carticon.innerHTML = bag.map((x) => x.quantity).reduce((x, y) => x + y, 0);
 };
 calculate();
 
+//asemenea paginii generate in timpul training ului, generam cartonasele din JS, intrucat
+//cartonasele afisate difera in functie de continutul array ului
 let generateCartItems = () => {
   if (bag.length !== 0) {
-    return (shopping_cart.innerHTML = bag
+    //avem de afisat cartonase doar daca lungimea array ului este mai mare ca 0
+    return (shopping_cart.innerHTML = bag //parcurgem fiecare element al array ului cu map,
+      // pentru a putea afisa fiecare cartonas in parte
       .map((x) => {
+        //facem destructuring pentru a accesa mai usor proprietatile obiectului
         let { id, quantity } = x;
         let cautat = dataItems.find((y) => y.id === id) || [];
         let { img, price, name, desc } = cautat;
-        //sa ii recomand sa fac destructuring
         return `
             <div  class="cart-bag ${name}">
             <img width="200" height="200" src="${img}" alt="" />
@@ -38,9 +65,10 @@ let generateCartItems = () => {
           </div>
             `;
       })
-      .join(""));
+      .join("")); //join pentru a scapa de virgula (vezi training)
   } else {
     shopping_cart.innerHTML = "";
+    //dorim sa informam utilizatorul in cazul in care acesta nu a adaugat nimic in cos, prin urmare, lasam un mesaj informativ
     label.innerHTML = `
      <div>
      <h2>Nu aveți niciun produs în coș momentan</h2></div>`;
@@ -48,6 +76,8 @@ let generateCartItems = () => {
 };
 generateCartItems();
 
+//trebuie sa putem mari cantitatea de produs si din pagina de cos, aplicam aceeasi
+//logica ca cea utilizata in timpul training-ului
 const increment = (id) => {
   let selected = id;
   let cautat = bag.find((x) => x.id === selected.id);
@@ -63,7 +93,15 @@ const increment = (id) => {
   generateCartItems();
   update(selected.id);
   localStorage.setItem("data", JSON.stringify(bag));
+  /*
+    localStorage.setItem(keyname, value) - pentru a seta valoarea obiectului setat in localstorage
+    noi vom actualiza de fiecare data consinutul array ului cu obiecte achizitionate
+  */
+  // keyname-ul : data
+  //value: formatul JSON al array-ului actualizat (salvam ultima versiune a cosului in urma modificarilor efectuate)
 };
+
+//idem increment
 const decrement = (id) => {
   let selected = id;
   let cautat = bag.find((x) => x.id === selected.id);
@@ -80,25 +118,31 @@ const decrement = (id) => {
   localStorage.setItem("data", JSON.stringify(bag));
 };
 
+//aceeasi logica utilizata in cadrul training-ului, vezi training
 const update = (id) => {
   let cautat = bag.find((x) => x.id === id);
   document.getElementById(id).innerHTML = cautat.quantity;
   calculate();
-  total();
+  total(); // de data asta apelam si total, pentru a actualiza si pretul total, cel afisat ca valoare finala a cosului
 };
 
+//METODA TOTAL
 let total = () => {
   if (bag.length !== 0) {
+    //parcurgem tot array ul, in aceleasi conditii ca pana acum
     let amount = bag
       .map((x) => {
+        //obtinem pretul * cantitatea comandata aferente fiecarui produs
         let { quantity, id } = x;
         let cautat = dataItems.find((y) => y.id === id) || [];
         return quantity * cautat.price;
       })
-      .reduce((x, y) => x + y, 0);
+      .reduce((x, y) => x + y, 0); //aduname valoarea fiecarui produs la amount, adica la totalul de plata
     label.innerHTML = `
         <h2>Total:................................. ${amount} $</h2>
-        `;
+        `; //in urma modificarilor este si afisat rezultatul final
   } else return;
 };
-total();
+total(); //apelam functia total pentru a putea avea un total afisat si
+//la prima randare a paginii, cand este facuta tranzitia dintre pagina cu produse propuse cumpararii
+//si pagina cu produsele selectate
